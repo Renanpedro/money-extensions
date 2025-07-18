b386c006eb13e58ec9153d2aa1ad0569cb96c229
Renan Silva <contato.renan.silva@outlook.com>
2025-07-18 11:36:22 -0300

    Finalizado a primeira versão

---
+++ MoneyExtension/MoneyExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace MoneyExtension;
+
+public static class Money
+{
+    public static int ToCents(this decimal Amount)
+    {
+        if (Amount < 0)
+            return 0;
+
+        var text = Amount
+                    .ToString("N2")
+                    .Replace(",", "")
+                    .Replace(".", "");
+                    
+        if (string.IsNullOrEmpty(text))
+            return 0;
+
+        int.TryParse(text, out var result);
+        return result;
+    }
+}
